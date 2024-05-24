4ef41e3ed2da36ae56150eb6462510dc888ded0f
mujl <1219757241@qq.com>
2024-05-24 22:30:24 +0800

    🦄 refactor: 去掉socket不正确回调

--- utils/socket.ts
+++ utils/socket.ts
@@ -1,12 +1,11 @@
 import { Server as HttpServer } from 'http';
 import { Server as SocketServer } from 'socket.io';
 
-import { WEB_PORT } from '../config';
 import logger from './logger';
 import userService from '../service/user';
 
 interface ServerToClientEvents {
-    join: (uid: string) => void;
+    members: (uidList: string[]) => void;
     offer: (uid: string, sdp: RTCSessionDescriptionInit) => void;
     answer: (uid: string, sdp: RTCSessionDescriptionInit) => void;
     candidate: (uid: string, candidate: RTCIceCandidate) => void;
@@ -15,16 +14,12 @@ interface ServerToClientEvents {
 }
 
 interface ClientToServerEvents {
-    join: (mid: string, callback: (idList: string[]) => void) => void;
+    join: (mid: string) => void;
     offer: (uid: string, sdp: RTCSessionDescriptionInit) => void;
     answer: (uid: string, sdp: RTCSessionDescriptionInit) => void;
     candidate: (uid: string, candidate: RTCIceCandidate) => void;
     removeStream: (uid: string, sid: string) => void;
 }
-
-interface InterServerEvents {}
-
-interface SocketData {}
 
 // map
 const idMapSid = new Map<string, string>();
@@ -32,16 +27,14 @@ const sidMapId = new Map<string, string>();
 
 /** 启动 Web Socket 服务 */
 function setupSocket(httpServer: HttpServer) {
-    const io = new SocketServer<
-        ClientToServerEvents,
-        ServerToClientEvents,
-        InterServerEvents,
-        SocketData
-    >(httpServer, {
-        cors: {
-            origin: '*',
+    const io = new SocketServer<ClientToServerEvents, ServerToClientEvents>(
+        httpServer,
+        {
+            cors: {
+                origin: '*',
+            },
         },
-    });
+    );
 
     // log
     io.of('/').adapter.on('join-room', (room, id) => {
@@ -59,71 +52,66 @@ function setupSocket(httpServer: HttpServer) {
         next();
     });
 
-    io.on('connection', async socket => {
+    io.on('connection', socket => {
         let mid: string;
         logger.info(`Socket ${socket.id} connected`);
 
-        // 加入房间
-        socket.on('join', async (room, cb) => {
-            const id = sidMapId.get(socket.id);
-            if (!id) return;
-            io.to(room).emit('join', id);
-            socket.join(room);
-            mid = room;
-            await userService.recordAttendance(mid, id);
-            const sockets = await io.in(room).fetchSockets();
-            const idList = sockets
-                .filter(curr => curr.id !== socket.id)
-                .reduce<string[]>((prev, curr) => {
-                    const id = sidMapId.get(curr.id);
-                    return id ? [...prev, id] : prev;
-                }, []);
-            cb(idList);
-        });
-
-        // 转发offer
-        socket.on('offer', (id, sdp) => {
-            const targetSid = idMapSid.get(id);
-            const sourceId = sidMapId.get(socket.id);
-            if (!targetSid || !sourceId) return;
-            io.to(targetSid).emit('offer', sourceId, sdp);
-            logger.debug(`${sourceId} offer to ${id}`);
-        });
-
-        // 转发answer
-        socket.on('answer', (id, sdp) => {
-            const targetSid = idMapSid.get(id);
-            const sourceId = sidMapId.get(socket.id);
-            if (!targetSid || !sourceId) return;
-            io.to(targetSid).emit('answer', sourceId, sdp);
-            logger.debug(`${sourceId} answer to ${id}`);
-        });
-
-        // 转发candidate
-        socket.on('candidate', (id, candidate) => {
-            const targetSid = idMapSid.get(id);
-            const sourceId = sidMapId.get(socket.id);
-            if (!targetSid || !sourceId) return;
-            io.to(targetSid).emit('candidate', sourceId, candidate);
-        });
-
-        // 关闭流
-        socket.on('removeStream', (uid, sid) => {
-            if (mid) {
-                io.to(mid).emit('removeStream', uid, sid);
-            }
-        });
-
-        socket.on('disconnect', () => {
-            logger.info(`Socket ${socket.id} disconnected`);
-            const id = sidMapId.get(socket.id);
-            if (!id) return;
-            idMapSid.delete(id);
-            sidMapId.delete(socket.id);
-            if (mid) {
-                io.to(mid).emit('leave', id);
-            }
-        });
+        socket
+            // 加入房间
+            .on('join', async room => {
+                const id = sidMapId.get(socket.id);
+                if (!id) return;
+                await socket.join(room);
+                mid = room;
+                await userService.recordAttendance(mid, id);
+                const sockets = await io.in(room).fetchSockets();
+                const uidList = sockets
+                    .filter(curr => curr.id !== socket.id)
+                    .reduce<string[]>((prev, curr) => {
+                        const id = sidMapId.get(curr.id);
+                        return id ? [...prev, id] : prev;
+                    }, []);
+                io.to(socket.id).emit('members', uidList);
+            })
+            // 转发offer
+            .on('offer', (id, offer) => {
+                const targetSid = idMapSid.get(id);
+                const sourceId = sidMapId.get(socket.id);
+                if (!targetSid || !sourceId) return;
+                io.to(targetSid).emit('offer', sourceId, offer);
+                logger.debug(`${sourceId} offer to ${id}`);
+            })
+            // 转发answer
+            .on('answer', (id, answer) => {
+                const targetSid = idMapSid.get(id);
+                const sourceId = sidMapId.get(socket.id);
+                if (!targetSid || !sourceId) return;
+                io.to(targetSid).emit('answer', sourceId, answer);
+                logger.debug(`${sourceId} answer to ${id}`);
+            })
+            // 转发candidate
+            .on('candidate', (id, candidate) => {
+                const targetSid = idMapSid.get(id);
+                const sourceId = sidMapId.get(socket.id);
+                if (!targetSid || !sourceId) return;
+                io.to(targetSid).emit('candidate', sourceId, candidate);
+            })
+            // 关闭流
+            .on('removeStream', (uid, sid) => {
+                if (mid) {
+                    io.to(mid).emit('removeStream', uid, sid);
+                }
+            })
+            .on('disconnect', () => {
+                logger.info(`Socket ${socket.id} disconnected`);
+                const id = sidMapId.get(socket.id);
+                if (!id) return;
+                idMapSid.delete(id);
+                sidMapId.delete(socket.id);
+                if (mid) {
+                    io.to(mid).emit('leave', id);
+                }
+            });
     });
 }
 
